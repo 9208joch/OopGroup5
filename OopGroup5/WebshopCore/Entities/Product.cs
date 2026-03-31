e2bbf129b03d12da3d690e17c777aa2362061bbb
9208joch <9208joch@gapp.uddevalla.se>
2026-03-31 17:03:02 +0200

    fixt fuckup

---
+++ OopGroup5/WebshopCore/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+
+    public string Supplier { get; set; }
+    public int Stock { get; set; }
+
+    public bool IsFeatured { get; set; }
+}
---
+++ OopGroup5/WebshopInfrastructure/WebShopContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class WebShopContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    public WebShopContext(DbContextOptions<WebShopContext> options)
+        : base(options) { }
+}
